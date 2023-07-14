e97337d6baeb79def0ddfa9ca7322cf09c91e224
Jonathan Nguyen <jonathanguven@gmail.com>
2023-07-14 15:04:30 -0700

    completed checkWeather helper command

--- checkWeather.js
+++ checkWeather.js
@@ -2,12 +2,13 @@
 module.exports = {
     checkWeather: async function checkWeather(query) {
         const apiKey = process.env.API_KEY
-        const url = `http://api.weatherapi.com/v1?key=${apiKey}?q=${query}`
+        const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${query}&aqi=yes}`
         const options = { 
             method: 'GET' 
         }
 
-        const response = await fetch(url, options);
+        const response = await fetch(url);
         const data = await response.json();
+        return data;
     }
 }
