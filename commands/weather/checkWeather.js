9d24da25ce58ab647992158d8c8f2f522fa34c6e
Jonathan Nguyen <jonathanguven@gmail.com>
2023-07-14 23:09:53 -0700

    added support for choosing locations

--- commands/weather/checkWeather.js
+++ commands/weather/checkWeather.js
@@ -1,12 +1,15 @@
-
+const defaultLocation = require('../../settings.json');
 module.exports = {
-    checkWeather: async function checkWeather(query) {
+    checkWeather: async function checkWeather(location) {
         const apiKey = process.env.API_KEY
-        const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${query}&days=1&ai=no&alerts=yes`
+        let loc = defaultLocation;
+        if (location !== null) {
+            loc = location;
+        }
+        const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=1&ai=no&alerts=yes`
         const options = { 
             method: 'GET' 
         }
-
         const response = await fetch(url);
         const data = await response.json();
         return data;
