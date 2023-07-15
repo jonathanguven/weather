e843ae7fbe8e0c0584cc61a3a239a49ab883f6e0
Jonathan Nguyen <jonathanguven@gmail.com>
2023-07-14 21:03:07 -0700

    completed weather and weather.js commands

--- checkWeather.js
+++ checkWeather.js
@@ -2,7 +2,7 @@
 module.exports = {
     checkWeather: async function checkWeather(query) {
         const apiKey = process.env.API_KEY
-        const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${query}&aqi=yes}`
+        const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${query}&days=1&ai=no&alerts=yes`
         const options = { 
             method: 'GET' 
         }
--- commands/weather/weather.js
+++ commands/weather/weather.js
@@ -10,33 +10,29 @@ module.exports = {
         .addStringOption(option => option.setName('degree').setDescription('The degree type (C or F)').addChoices({ name: 'F', value: 'F'}, { name: 'C', value: 'C'})),
     async execute(interaction) {
 
-        
-        const data = await weather.checkWeather('San Jose');
-        console.log(data);
 
-        const temp = 'test';
-        const type = 'test';
-        const name = 'test';
-        const feel = 'test';
-        const icon = 'test';
-        const wind = 'test';
-        const day = 'test';
-        const alert = 'test';
+        const data = await weather.checkWeather('San Jose');
+        const temp = data.current.temp_f;
+        const condition = data.current.condition.text;
+        const name = data.location.name;
+        const feel = data.current.feelslike_f;
+        const icon = `https:${data.current.condition.icon}`;
+        const windSpeed = data.current.wind_mph;
+        const windDir = data.current.wind_dir;
+        
 
 
 
         const weatherEmbed = new EmbedBuilder()
         .setColor(0x0099FF)
-        .setTitle(`Current weather of ${name}`)
+        .setTitle(`Current weather of ${name}, ${data.location.region}`)
         .addFields( { name : 'Temperature', value: `${temp}`})
         .addFields( { name : 'Feels like', value: `${feel}`})
-        .addFields( { name : 'Weather', value: `${type}`})
-        .addFields( { name : 'Current Alerts', value: `${alert}`})
-        .addFields( { name : 'Week Day', value: `${day}`})
-        .addFields( { name : 'Wind Speed & Direction', value: `${wind}`})
-        // .setThumbnail(icon)
+        .addFields( { name : 'Weather', value: `${condition}`})
+        .addFields( { name : 'Wind Speed & Direction', value: `${windSpeed}, ${windDir}`})
+        .setThumbnail(icon)
         .setTimestamp()
-        .setFooter({ text: 'Thank you for using weather bot'});
+        .setFooter({ text: 'Data pulled from weatherapi.com'});
         await interaction.reply({ embeds: [weatherEmbed] });
     }
 };
