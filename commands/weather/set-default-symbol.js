3210730c44a3332177c6ee65f1ff7ba6028b9e29
Jonathan Nguyen <jonathanguven@gmail.com>
2023-07-17 23:29:41 -0700

    minor bug fixes for settings command

--- commands/weather/set-default-symbol.js
+++ commands/weather/set-default-symbol.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-let symbol = 'F'; // default location is null until user runs this command
+let symbol = 'F'; 
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,12 +8,13 @@ module.exports = {
         .addStringOption(option => option
             .setName('symbol')
             .setDescription('\'F\' for Fahrenheit, \'C\' for Celcius')
+            .addChoices({ name: 'F', value: 'F'}, { name: 'C', value: 'C'})
             .setRequired(true)),
-    getSymbol: async function getSymbol() { // helper function for weather.js command to retrieve default location
+    getSymbol: async function getSymbol() { // helper function for weather.js command to retrieve sign of temp
         return symbol;
     },
     async execute(interaction) {
-        symbol = interaction.options.getString('symbol') === 'F' ? 'F' : 'C'; // sets new default location from user input
+        symbol = interaction.options.getString('symbol') === 'F' ? 'F' : 'C'; // sets new symbol from user input
         let symbolVal = (symbol === 'F') ? 'Fahrenheit' : 'Celcius';
         return interaction.reply(`Default temperature scale set to ${symbolVal}`); 
     },
--- commands/weather/settings.js
+++ commands/weather/settings.js
@@ -7,7 +7,9 @@ module.exports = {
         .setName('settings')
         .setDescription('Provides information on current presets'),
     async execute(interaction) {
-        const sign = symbol === 'F' ? 'Fahrenheit' : 'Celcius';
-        return interaction.reply(`Default Location: ${location}\nDefault Symbol: ${sign}`);
+        let symbolVal = (await symbol.getSymbol() === 'F') ? 'Fahrenheit' : 'Celcius';
+        const loc = await location.getLocation();
+        let city = loc === null ? 'No default location set' : loc;
+        return interaction.reply(`Default Location: ${city}\nDefault Symbol: ${symbolVal}`);
     },
 };
