d81e2e13145bf264c04ab4bdda9446c7f2a9b833
Jonathan Nguyen <jonathanguven@gmail.com>
2023-07-17 23:29:27 -0700

    error handling for invalid location requests

--- checkWeather.js
+++ checkWeather.js
@@ -1,12 +1,14 @@
 module.exports = {
     checkWeather: async function checkWeather(location) {
-        const apiKey = process.env.API_KEY
-        const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=1&ai=no&alerts=yes`
-        const options = { 
-            method: 'GET' 
+        try {
+            const apiKey = process.env.API_KEY
+            const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=1&ai=no&alerts=yes`;
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        } catch (err) {
+            console.log(err);
+            throw err;
         }
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
     }
 }
--- commands/weather/weather.js
+++ commands/weather/weather.js
@@ -19,37 +19,40 @@ module.exports = {
         if (city === null) {
             return interaction.reply('Please provide a valid location, or set a new default location'); // default location has not yet been set
         }
-
         let degree = interaction.options.getString('degree') // grabs C or F from user input
         if (!degree) { degree = await symbol.getSymbol(); }
+        try { // catch if city is not a real place and returns no data from api call
+            // api call
+            const data = await weather.checkWeather(city);
 
-        // api call
-        const data = await weather.checkWeather(city);
+            let temp = data.current.temp_f;;
+            let feel = data.current.feelslike_f;;
+            if (degree === 'C') {
+                temp = data.current.temp_c;
+                feel = data.current.feelslike_c;
+            }
 
-        let temp = data.current.temp_f;;
-        let feel = data.current.feelslike_f;;
-        if (degree === 'C') {
-            temp = data.current.temp_c;
-            feel = data.current.feelslike_c;
+            const condition = data.current.condition.text;
+            const name = data.location.name;
+            const icon = `https:${data.current.condition.icon}`;
+            const windSpeed = data.current.wind_mph;
+            const windDir = data.current.wind_dir;
+
+            const weatherEmbed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle(`Current weather of ${name}, ${data.location.region}`)
+            .addFields( { name : 'Temperature', value: `${temp} \xB0${degree}`})
+            .addFields( { name : 'Feels like', value: `${feel} \xB0${degree}`})
+            .addFields( { name : 'Weather', value: `${condition}`})
+            .addFields( { name : 'Wind Speed & Direction', value: `${windSpeed}, ${windDir}`})
+            .setThumbnail(icon)            
+            .setTimestamp()
+            .setFooter({ text: 'Data pulled from weatherapi.com'});
+            await interaction.reply({ embeds: [weatherEmbed] });
+        } catch (error) {
+            await interaction.reply(`An error occured while checking the weather of ${city}.\nPlease use a valid location\n`);
+            console.log(error);
         }
-
-        const condition = data.current.condition.text;
-        const name = data.location.name;
-        const icon = `https:${data.current.condition.icon}`;
-        const windSpeed = data.current.wind_mph;
-        const windDir = data.current.wind_dir;
-
-        const weatherEmbed = new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setTitle(`Current weather of ${name}, ${data.location.region}`)
-        .addFields( { name : 'Temperature', value: `${temp} \xB0${degree}`})
-        .addFields( { name : 'Feels like', value: `${feel} \xB0${degree}`})
-        .addFields( { name : 'Weather', value: `${condition}`})
-        .addFields( { name : 'Wind Speed & Direction', value: `${windSpeed}, ${windDir}`})
-        .setThumbnail(icon)            
-        .setTimestamp()
-        .setFooter({ text: 'Data pulled from weatherapi.com'});
-        await interaction.reply({ embeds: [weatherEmbed] });
         
-    }
+    } 
 };
